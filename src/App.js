8d92f3d023963d61a0ec29544474cf36c70fd053
Matthieu Caron <104826273+matthieucarondev@users.noreply.github.com>
2024-07-04 10:19:39 +0200

    creat react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        librairy modal
         </p>
         <a
           className="App-link"
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          salut
+        
         </a>
       </header>
     </div>
